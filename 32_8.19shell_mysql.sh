04d8cb31700e49f1dcecbbe0b9eb7dbfb7d03224
zhangwenlong02 <429597859@qq.com>
2017-08-18 23:23:49 -0700

    shell_mysql

--- 32_8.19shell_mysql.sh
+++ 32_8.19shell_mysql.sh
@@ -1,4 +1,4 @@
-#! /bin/hash
+#! /bin/bash
 #shell_mysql.sh
 cat<<a
 	1.数据可基本操作
@@ -7,3 +7,11 @@ cat<<a
 	4.日志分割之任务计划
 	5.apache日志统计之myaql数据库
 a
+mysql="/usr/local/mysql/bin/mysql -uroot -p123"
+sql="creat table zwl.user(
+	in intl unsignen auto_increment rpimary key,
+	username varchar(50)
+	password varchare(50)
+)"
+#now="show tables from test"
+$mysql -e "$sql"
