e8b8970272ed282463cb12ab494369d913eb44d5
zhangwenlong02 <429597859@qq.com>
2017-08-21 06:37:03 -0700

    modifie file message

--- 4_8.21mysql_design.sh
+++ 4_8.21mysql_design.sh
@@ -1,10 +1,15 @@
 常用数据类型
 数值类型
+tinyint -127<=数值<=127
 int 有符号
 int unsigned 无符号
+bigint 有符号
 
 字符串类型
 char(255) 长度≤255
 varchar(255) 长度≤255
 text  255＜长度≤65535
 
+char和varchar区别
+1.char最长255，varchar最长65535
+2.char固定长度空间，carchar可变空间
