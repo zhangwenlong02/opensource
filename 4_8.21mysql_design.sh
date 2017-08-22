f13d09088c1776e18d5a66dcda963858568e74aa
zhangwenlong02 <429597859@qq.com>
2017-08-21 17:14:14 -0700

    data type数据字段类型

--- 4_8.21mysql_design.sh
+++ 4_8.21mysql_design.sh
@@ -13,3 +13,22 @@ text  255＜长度≤65535
 char和varchar区别
 1.char最长255，varchar最长65535
 2.char固定长度空间，carchar可变空间
+
+数据字段类型
+1.unsigned
+2.zerofiill  0填充（默认设置成无符号）
+3.auto_increment primary key(自动增加,需添加主键索引'primary key')
+4.null
+5.not null
+6.default
+主键自增的两种方式：
+te table t4( 
+	id int unsigned auto_increment primary key, 
+	name varchar(10)
+	);
+ create table t5( 
+	id int unsigned auto_increment,
+	name varchar(20),
+	primary key(id)
+	);
+null和默认值没有任何关系
